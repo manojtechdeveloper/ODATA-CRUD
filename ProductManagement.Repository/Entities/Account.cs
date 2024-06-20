373f7387ecd740667c7fb3f3ff872d4542b37a91
manojtechdeveloper <127630436+manojtechdeveloper@users.noreply.github.com>
2024-06-20 11:24:29 +0530

    Update Account.cs

--- ProductManagement.Repository/Entities/Account.cs
+++ ProductManagement.Repository/Entities/Account.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,36 @@ using System.Threading.Tasks;
 
 namespace ProductManagement.Repository.Entities
 {
+    /// <summary>
+    /// Represents a bank account.
+    /// </summary>
     public class Account
     {
+        /// <summary>
+        /// Unique identifier for the account.
+        /// </summary>
         public int AccountId { get; set; }
+
+        /// <summary>
+        /// Name associated with the account.
+        /// </summary>
         public string AccountName { get; set; }
+
+        /// <summary>
+        /// Current balance in the account.
+        /// </summary>
         public decimal Balance { get; set; }
+
+        /// <summary>
+        /// Type of the account (e.g., savings, checking).
+        /// </summary>
         public string AccountType { get; set; }
 
-        // Navigation property for transations 
-        public ICollection<Transaction> Transactions { get; set; }  
-
+        /// <summary>
+        /// Collection of transactions associated with this account.
+        /// </summary>
+        public ICollection<Transaction> Transactions { get; set; }
     }
-     
+
+
 }
