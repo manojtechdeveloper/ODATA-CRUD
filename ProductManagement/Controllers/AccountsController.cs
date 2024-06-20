14c6a1488da80076321cedf61715b0b97cabdc9f
manojtechdeveloper <127630436+manojtechdeveloper@users.noreply.github.com>
2024-06-20 12:07:32 +0530

    Update AccountsController.cs

--- ProductManagement/Controllers/AccountsController.cs
+++ ProductManagement/Controllers/AccountsController.cs
@@ -5,7 +5,6 @@ using ProductManagement.Repository.Entities;
 using ProductManagement.Service.IServices;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace ProductManagement.Controllers
 {
@@ -20,22 +19,31 @@ namespace ProductManagement.Controllers
             _accountService = accountService ?? throw new ArgumentNullException(nameof(accountService));
         }
 
+        /// <summary>
+        /// Returns a list of all accounts.
+        /// </summary>
+        /// <returns>A list of all accounts.</returns>
         [HttpGet]
         [EnableQuery]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Account>))]
-        public async Task<ActionResult<IEnumerable<Account>>> Get()
+        public ActionResult<IEnumerable<Account>> Get()
         {
-            var accounts = await _accountService.GetAccountsAsync();
+            var accounts = _accountService.GetAccountsAsync().Result;
             return Ok(accounts);
         }
 
+        /// <summary>
+        /// Returns an account by its ID.
+        /// </summary>
+        /// <param name="key">The ID of the account.</param>
+        /// <returns>The account details.</returns>
         [HttpGet("{key}")]
         [EnableQuery]
         [ProducesResponseType(200, Type = typeof(Account))]
         [ProducesResponseType(404)]
-        public async Task<ActionResult<Account>> Get([FromRoute] int key)
+        public ActionResult<Account> Get([FromRoute] int key)
         {
-            var account = await _accountService.GetAccountByIdAsync(key);
+            var account = _accountService.GetAccountByIdAsync(key).Result;
             if (account == null)
             {
                 return NotFound();
@@ -43,42 +51,58 @@ namespace ProductManagement.Controllers
             return Ok(account);
         }
 
+        /// <summary>
+        /// Creates a new account.
+        /// </summary>
+        /// <param name="account">The account to create.</param>
+        /// <returns>The ID of the created account.</returns>
         [HttpPost]
         [ProducesResponseType(200, Type = typeof(int))]
         [ProducesResponseType(400)]
-        public async Task<ActionResult<int>> Post([FromBody] Account account)
+        public ActionResult<int> Post([FromBody] Account account)
         {
             if (account == null)
             {
                 return BadRequest("Account data is null.");
             }
-
             _accountService.CreateAccountAsync(account);
             return Ok(account.AccountId);
         }
 
+        /// <summary>
+        /// Updates an existing account by ID.
+        /// </summary>
+        /// <param name="key">The ID of the account to update.</param>
+        /// <param name="account">The updated details of the account.</param>
+        /// <returns>The ID of the updated account.</returns>
         [HttpPut("{key}")]
         [ProducesResponseType(200, Type = typeof(int))]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        public async Task<ActionResult<int>> Put([FromRoute] int key, [FromBody] Account account)
+        public ActionResult<int> Put([FromRoute] int key, [FromBody] Account account)
         {
             if (account == null || key != account.AccountId)
             {
                 return BadRequest("Invalid account data or ID mismatch.");
             }
-
-            await _accountService.UpdateAccountAsync(key, account);
+            _accountService.UpdateAccountAsync(key, account);
             return Ok(account.AccountId);
         }
 
+        /// <summary>
+        /// Deletes an account by ID.
+        /// </summary>
+        /// <param name="key">The ID of the account to delete.</param>
+        /// <returns>The ID of the deleted account.</returns>
         [HttpDelete("{key}")]
         [ProducesResponseType(200, Type = typeof(int))]
         [ProducesResponseType(404)]
-        public async Task<ActionResult<int>> Delete([FromRoute] int key)
+        public ActionResult<int> Delete([FromRoute] int key)
         {
-            await _accountService.DeleteAccountAsync(key);
+            _accountService.DeleteAccountAsync(key);
             return Ok(key);
+
         }
     }
 }
+
