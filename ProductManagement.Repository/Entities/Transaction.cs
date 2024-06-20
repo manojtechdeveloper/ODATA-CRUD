87f344fa63b1a0c64ba7fe6bc406e6c09dd1fbf9
manojtechdeveloper <127630436+manojtechdeveloper@users.noreply.github.com>
2024-06-20 11:24:53 +0530

    Update Transaction.cs

--- ProductManagement.Repository/Entities/Transaction.cs
+++ ProductManagement.Repository/Entities/Transaction.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,40 @@ using System.Threading.Tasks;
 
 namespace ProductManagement.Repository.Entities
 {
+    /// <summary>
+    /// Represents a financial transaction.
+    /// </summary>
     public class Transaction
     {
-        public int TransactionId { get; set; }  
+        /// <summary>
+        /// Unique identifier for the transaction.
+        /// </summary>
+        public int TransactionId { get; set; }
+
+        /// <summary>
+        /// Description of the transaction.
+        /// </summary>
         public string Description { get; set; }
-        public DateTime Date {  get; set; }
+
+        /// <summary>
+        /// Date and time when the transaction occurred.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Identifier of the source account for the transaction.
+        /// </summary>
         public int SourceAccountID { get; set; }
+
+        /// <summary>
+        /// Identifier of the destination account for the transaction.
+        /// </summary>
         public int DestinationAccountID { get; set; }
 
-        //Foreign keys 
+        /// <summary>
+        /// Foreign key referencing the account involved in the transaction.
+        /// </summary>
         public int AccountId { get; set; }
-       // public Account Account { get; set; }
     }
+
 }
